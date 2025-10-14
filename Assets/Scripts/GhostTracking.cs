70176e325ce93365cd69526359874eeadc72e4c1
Kai Lee <kjleesf@gmail.com>
2025-10-14 17:59:42 -0400

    ghost logic

--- Assets/Scripts/GhostTracking.cs
+++ Assets/Scripts/GhostTracking.cs
@@ -2,7 +2,11 @@ using UnityEngine;
 
 public class GhostTracking : MonoBehaviour
 {
-    int speed;
+    float speed = 2f;
+
+    Vector2 direction;
+
+    Vector2 velocity;
 
     Rigidbody2D rb;
 
@@ -11,15 +15,59 @@ public class GhostTracking : MonoBehaviour
     [SerializeField]
     GameObject player;
 
+    SpriteRenderer sprite;
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        playerPos = player.transform.position;
+        sprite = GetComponent<SpriteRenderer>();
     }
 
 
     void Update()
     {
-        
+        playerPos = player.transform.position;
+
+        if (playerPos.x < rb.position.x)
+        {
+            direction.x = -2;
+        }
+        else if (playerPos.x > rb.position.x)
+        {
+            direction.x = 2;
+        }
+        if (playerPos.y < rb.position.y)
+        {
+            direction.y = -1.3f;
+        }
+        else if (playerPos.y > rb.position.y)
+        {
+            direction.y = 1.3f;
+        }
+        else if (playerPos.y == rb.position.y)
+        {
+            direction.y = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (direction.magnitude > 0)
+        {
+            velocity = direction * speed * Time.fixedDeltaTime;
+            Vector2 newPos = (Vector2)transform.position + velocity;
+            rb.MovePosition(newPos);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //collision.gameObject.SetActive(false);
+        sprite.color = Color.red;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        sprite.color = Color.white;
     }
 }
