35870d12800716a83c1660fef4e39527383ad123
NolanK68 <nlk6882@rit.edu>
2025-10-15 18:29:33 -0400

    Outer lense stage 1 finished
    Finished outer lense detection - may be turned off in the inspector under hitbox > outerlense > hitbox

---
+++ Assets/Scripts/InBoxDisplay.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class InBoxDisplay : MonoBehaviour
+{
+
+    [SerializeField]
+    bool isActive;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+            collision.transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (isActive)
+        {
+            collision.transform.position = new Vector3(collision.transform.position.x, collision.transform.position.y, -20);
+        }
+    }
+
+}
--- Assets/Scripts/OuterLenseTracking.cs
+++ Assets/Scripts/OuterLenseTracking.cs
@@ -1,7 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class OuterLenseTracking : MonoBehaviour
 {
+
+    [SerializeField]
+    Vector2 ObjPos;
+
+    [SerializeField]
+    Vector2 mousePosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,10 @@ public class OuterLenseTracking : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        ObjPos = this.transform.position;
+
+        mousePosition = Mouse.current.position.ReadValue();
+        mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(mousePosition);
+        this.transform.position = mousePosition;
     }
 }
