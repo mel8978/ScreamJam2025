278707bd2da20136e8351920ada7135ea683a549
Michael Lavella <mel8978@g.rit.edu>
2025-10-15 15:42:09 -0400

    Mouse controller updates
    Mouse can now kill ghosts on click,
    added stage borders and temporary platforms

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -4,11 +4,18 @@ using UnityEngine.InputSystem;
 public class MouseController : MonoBehaviour
 {
     Vector2 mousePosition;
+    BoxCollider2D boxCollider;
+    SpriteRenderer spriteRenderer;
+
+    float timer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        boxCollider = GetComponent<BoxCollider2D>();
+        boxCollider.enabled = false;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        timer = 0;
     }
 
     // Update is called once per frame
@@ -17,5 +24,33 @@ public class MouseController : MonoBehaviour
         mousePosition = Mouse.current.position.ReadValue();
         mousePosition = (Vector2)Camera.main.ScreenToWorldPoint(mousePosition);
         transform.position = mousePosition;
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer <= 0 && boxCollider.enabled)
+        {
+            boxCollider.enabled = false;
+            spriteRenderer.color = Color.white;
+        }
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Performed && timer <= 0)
+        {
+            timer = 1;
+            boxCollider.enabled = true;
+            spriteRenderer.color = Color.green;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ghost")
+        {
+            Destroy(collision.gameObject);
+        }        
     }
 }
